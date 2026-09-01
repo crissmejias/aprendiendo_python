64cd9a63021b6e602f2a8ede8273cdcc80eef2e3
Cristhian Mejias Serrano <cristhianmejiasserrano@gmail.com>
2026-09-01 12:54:38 -0500

    chore: configuración de linter, fix y formatting con ruff

--- fase_5/integracion_con_db/db.py
+++ fase_5/integracion_con_db/db.py
@@ -1,51 +1,52 @@
-import psycopg2
 import os
+
+import psycopg2
 from dotenv import load_dotenv
 
-def conectar_a_bd(bd,autocommit):
+
+def conectar_a_bd(bd, autocommit):
     load_dotenv()
     conn = psycopg2.connect(
-    database=bd,
-    user="postgres",
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST","localhost"),
-    port="5432"
+        database=bd,
+        user="postgres",
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST", "localhost"),
+        port="5432",
     )
     if autocommit:
         conn.autocommit = True
     cursor = conn.cursor()
-    return conn,cursor
+    return conn, cursor
+
 
 def crear_bd(nombre_bd):
     conn = None
     try:
-        conn,cursor = conectar_a_bd("postgres",True)
-        cursor.execute(f'''DROP DATABASE IF EXISTS {nombre_bd}''')
-        sql= f'''CREATE DATABASE {nombre_bd}'''
+        conn, cursor = conectar_a_bd("postgres", True)
+        cursor.execute(f"""DROP DATABASE IF EXISTS {nombre_bd}""")
+        sql = f"""CREATE DATABASE {nombre_bd}"""
         cursor.execute(sql)
         print(f"Se ha creado la base de datos '{nombre_bd}'")
-    except psycopg2.errors.OperationalError:
-        print("Hubo un problema con el servidor de bases de datos")
     except psycopg2.errors.ObjectInUse:
         print("La base de datos está en uso")
     finally:
         if conn:
             conn.close()
+
 
 def borrar_bd(nombre_bd):
     conn = None
     try:
-        conn,cursor = conectar_a_bd("postgres",True)
-        sql= f'''DROP DATABASE IF EXISTS {nombre_bd}'''
+        conn, cursor = conectar_a_bd("postgres", True)
+        sql = f"""DROP DATABASE IF EXISTS {nombre_bd}"""
         cursor.execute(sql)
         print(f"Se ha eliminado la base de datos '{nombre_bd}'")
-    except psycopg2.errors.OperationalError:
-        print("Hubo un error con el servidor de bases de datos")
     except psycopg2.errors.ObjectInUse:
         print("La base de datos está en uso")
     finally:
         if conn:
             conn.close()
+
+
 if __name__ == "__main__":
     crear_bd("testing_tareas")
-
